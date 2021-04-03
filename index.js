bebc629fd849c25586dd7a5a4c7941b1a15b90a7
Mohsin <mohsinafzal29@gmail.com>
2021-04-03 15:12:06 +0200

    Write the core logic of the library in index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const Numbers = {
+  hundred: 2,
+  thousand: 3,
+  million: 6,
+  billion: 9,
+  trillion: 12,
+  quadrillion: 15,
+  quintillion: 18,
+  sextillion: 21,
+  septillion: 24,
+  octillion: 27,
+  nonillion: 30,
+  decillion: 33,
+};
+
+for (let number in Numbers) {
+  module.exports = Number.prototype[number] = function () {
+    const result = this * Math.pow(10, Numbers[number]);
+    if (isNaN(result)) throw new Error("please pass a valid number");
+    return result;
+  };
+}
